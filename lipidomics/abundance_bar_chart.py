75e2657b30592041bf21a52d64ba5d4600fd3e02
Hamed Abdi <ha.abdii@gmail.com>
2024-06-25 18:04:22 -0400

    Enhance log2 scale plotting and improve user feedback
    - Refine log2 std calculation for better representation of data spread
    - Add detailed explanation for classes removed in log2 scale plot
    - Update docstrings for improved code documentation
    - Replace matplotlib figtext with st.write for Streamlit compatibility
    - Improve error handling and edge case management in log calculations
    
    This commit enhances data visualization accuracy and provides clearer
    feedback to users about data processing in log2 scale plots.

--- lipidomics/abundance_bar_chart.py
+++ lipidomics/abundance_bar_chart.py
@@ -5,15 +5,15 @@ import streamlit as st
 
 class AbundanceBarChart:
     """
-        A class for generating abundance bar charts from lipidomics data.
-    
-        This class processes lipidomics data to calculate and visualize the total 
-        abundance of lipid classes under selected experimental conditions. It supports 
-        different modes of data representation (linear or logarithmic scale) and provides 
-        functionalities for grouping data, calculating mean and standard deviation, 
-        handling log transformations, and filtering based on selected classes. The class 
-        also includes methods for customizing and rendering the final plot.
-        """
+    A class for generating abundance bar charts from lipidomics data.
+
+    This class processes lipidomics data to calculate and visualize the total 
+    abundance of lipid classes under selected experimental conditions. It supports 
+    different modes of data representation (linear or logarithmic scale) and provides 
+    functionalities for grouping data, calculating mean and standard deviation, 
+    handling log transformations, and filtering based on selected classes. The class 
+    also includes methods for customizing and rendering the final plot.
+    """
 
     @staticmethod
     @st.cache_data
@@ -21,11 +21,11 @@ class AbundanceBarChart:
         """
         Aggregates and computes mean and standard deviation for selected lipid classes 
         across specific conditions using provided sample lists.
-    
+
         This method groups the data by lipid classes and calculates the mean and standard 
         deviation of the mean areas for each condition, then computes log2 values for 
         improved visualization and analysis. The results are filtered for the selected classes.
-    
+
         Parameters:
         df (pd.DataFrame): The DataFrame containing lipidomics data.
         full_samples_list (list): List of all sample names in the dataset.
@@ -33,7 +33,7 @@ class AbundanceBarChart:
         conditions_list (list): List of experimental conditions.
         selected_conditions (list of str): Conditions selected for analysis.
         selected_classes (list of str): Lipid classes selected for analysis.
-    
+
         Returns:
         pd.DataFrame: DataFrame with aggregated mean, standard deviation, and log2 values 
                       for the selected classes and conditions.
@@ -49,11 +49,11 @@ class AbundanceBarChart:
     def group_and_sum(df, full_samples_list):
         """
         Groups and sums the mean area values for each lipid class based on the full sample list.
-    
+
         Parameters:
         df (pd.DataFrame): The DataFrame containing lipidomics data.
         full_samples_list (list): List of all sample names in the dataset.
-    
+
         Returns:
         pd.DataFrame: DataFrame grouped by 'ClassKey' with summed mean areas.
         """
@@ -63,10 +63,11 @@ class AbundanceBarChart:
     def calculate_mean_std_for_conditions(grouped_df, individual_samples_list, conditions_list, selected_conditions):
         """
         Calculates mean and standard deviation for specific conditions in the grouped DataFrame.
-    
+
         Parameters:
         grouped_df (pd.DataFrame): DataFrame grouped by lipid classes.
-        experiment: An object containing experimental details, including sample lists.
+        individual_samples_list (list of list): List of sample names categorized by condition.
+        conditions_list (list): List of experimental conditions.
         selected_conditions (list of str): Conditions selected for analysis.
         """
         for condition in selected_conditions:
@@ -81,30 +82,49 @@ class AbundanceBarChart:
     def calculate_log2_values(grouped_df, selected_conditions):
         """
         Computes log2 transformed mean and standard deviation values for selected conditions.
-    
+
         Parameters:
         grouped_df (pd.DataFrame): DataFrame with mean and standard deviation values.
         selected_conditions (list of str): Conditions selected for analysis.
+
+        Returns:
+        tuple: A tuple containing the filtered DataFrame and a list of removed classes.
         """
         for condition in selected_conditions:
             mean_col = f"mean_AUC_{condition}"
             std_col = f"std_AUC_{condition}"
             if mean_col in grouped_df.columns and std_col in grouped_df.columns:
-                grouped_df[f"log2_mean_AUC_{condition}"] = np.log2(grouped_df[mean_col].replace(0, np.nan)).replace(-np.inf, np.nan)
-                grouped_df[f"log2_std_AUC_{condition}"] = (
-                    np.log2(grouped_df[mean_col] + grouped_df[std_col]).replace(-np.inf, np.nan) - 
-                    np.log2(grouped_df[mean_col] - grouped_df[std_col]).replace(-np.inf, np.nan)
-                ).replace(-np.inf, np.nan) / 2
+                log2_mean_col = f"log2_mean_AUC_{condition}"
+                log2_std_col = f"log2_std_AUC_{condition}"
+                
+                grouped_df[log2_mean_col] = np.where(grouped_df[mean_col] > grouped_df[std_col],
+                                                     np.log2(grouped_df[mean_col]),
+                                                     np.nan)
+                grouped_df[log2_std_col] = np.where(grouped_df[mean_col] > grouped_df[std_col],
+                                                    (np.log2(grouped_df[mean_col] + grouped_df[std_col]) - 
+                                                     np.log2(grouped_df[mean_col] - grouped_df[std_col])) / 2,
+                                                    np.nan)
+        
+        # Count valid conditions for each class
+        valid_condition_count = grouped_df[[f"log2_mean_AUC_{condition}" for condition in selected_conditions]].notna().sum(axis=1)
+        
+        # Filter classes with at least two valid conditions
+        filtered_df = grouped_df[valid_condition_count >= 2]
+        
+        # Identify removed classes
+        removed_classes = grouped_df[valid_condition_count < 2]['ClassKey'].tolist()
+        
+        return filtered_df, removed_classes
 
     @staticmethod
     def filter_by_selected_classes(grouped_df, selected_classes):
         """
         Filters the DataFrame to include only the selected lipid classes.
-    
+
         Parameters:
         grouped_df (pd.DataFrame): The DataFrame containing aggregated data.
         selected_classes (list of str): Lipid classes selected for analysis.
-    
+
         Returns:
         pd.DataFrame: Filtered DataFrame containing only data for selected lipid classes.
         """
@@ -114,32 +134,51 @@ class AbundanceBarChart:
     def create_abundance_bar_chart(df, full_samples_list, individual_samples_list, conditions_list, selected_conditions, selected_classes, mode):
         """
         Creates an abundance bar chart for the selected lipid classes and conditions.
-    
+
         Parameters:
         df (pd.DataFrame): The DataFrame containing lipidomics data.
-        experiment: An object containing experimental details, including sample lists.
+        full_samples_list (list): List of all sample names in the dataset.
+        individual_samples_list (list of list): List of sample names categorized by condition.
+        conditions_list (list): List of experimental conditions.
         selected_conditions (list of str): Conditions selected for analysis.
         selected_classes (list of str): Lipid classes selected for analysis.
         mode (str): The mode for value calculation ('linear scale' or 'log2 scale').
-    
+
         Returns:
-        matplotlib.figure.Figure: A figure object containing the bar chart.
-        pd.DataFrame: A DataFrame used for the plot.
+        tuple: A tuple containing the matplotlib figure and the DataFrame used for plotting.
         """
         abundance_df = AbundanceBarChart.create_mean_std_columns(df, full_samples_list, individual_samples_list, conditions_list, selected_conditions, selected_classes)
-        fig, ax = AbundanceBarChart.initialize_plot(len(selected_classes))
+        
+        if mode == 'log2 scale':
+            abundance_df, removed_classes = AbundanceBarChart.calculate_log2_values(abundance_df, selected_conditions)
+        else:
+            removed_classes = []
+        
+        fig, ax = AbundanceBarChart.initialize_plot(len(abundance_df))
         AbundanceBarChart.add_bars_to_plot(ax, abundance_df, selected_conditions, mode)
         AbundanceBarChart.style_plot(ax, abundance_df)
+        
+        # Add note about removed classes if any
+        if removed_classes:
+            note = (
+                f"Note: The following classes were removed from the log2 scale plot due to insufficient valid data: {', '.join(removed_classes)}. "
+                "Insufficient valid data means that for these classes, fewer than two of the selected conditions had a mean value greater than its standard deviation. "
+                "This occurs when the data for these classes shows high variability relative to its average, "
+                "which can lead to unreliable or undefined results in log2 scale. "
+                "These classes are still included in the linear scale plot if available."
+            )
+            st.write(note)
+        
         return fig, abundance_df
 
     @staticmethod
     def initialize_plot(num_classes):
         """
         Initializes a matplotlib plot with a white background and dynamic figure size.
-    
+
         Parameters:
         num_classes (int): The number of lipid classes to be plotted.
-    
+
         Returns:
         tuple: A tuple containing a figure and axis object of the plot.
         """
@@ -154,7 +193,7 @@ class AbundanceBarChart:
     def add_bars_to_plot(ax, abundance_df, selected_conditions, mode):
         """
         Adds horizontal bars to the plot for selected conditions and mode.
-    
+
         Parameters:
         ax (matplotlib.axes.Axes): The axes object to add bars to.
         abundance_df (pd.DataFrame): DataFrame with calculated mean and std values.
@@ -180,12 +219,12 @@ class AbundanceBarChart:
     def get_mode_specific_values(abundance_df, condition, mode):
         """
         Retrieves mean and standard deviation values based on the selected mode.
-    
+
         Parameters:
         abundance_df (pd.DataFrame): DataFrame with calculated mean and std values.
         condition (str): Specific condition for which to retrieve values.
         mode (str): The mode for value calculation ('linear scale' or 'log2 scale').
-    
+
         Returns:
         tuple: A tuple containing mean and standard deviation values.
         """
@@ -193,8 +232,6 @@ class AbundanceBarChart:
             mean = abundance_df.get(f"mean_AUC_{condition}", pd.Series(dtype=float)).fillna(0)
             std = abundance_df.get(f"std_AUC_{condition}", pd.Series(dtype=float)).fillna(0)
         elif mode == 'log2 scale':
-            if f"log2_mean_AUC_{condition}" not in abundance_df.columns:
-                AbundanceBarChart.calculate_log2_values(abundance_df, [condition])
             mean = abundance_df.get(f"log2_mean_AUC_{condition}", pd.Series(dtype=float)).fillna(0)
             std = abundance_df.get(f"log2_std_AUC_{condition}", pd.Series(dtype=float)).fillna(0)
         return mean, std
@@ -203,7 +240,7 @@ class AbundanceBarChart:
     def style_plot(ax, abundance_df):
         """
         Styles the plot with labels, title, legend, and a frame.
-    
+
         Parameters:
         ax (matplotlib.axes.Axes): The axes object to style.
         abundance_df (pd.DataFrame): DataFrame used for the plot.
@@ -211,10 +248,8 @@ class AbundanceBarChart:
         ax.set_xlabel('Mean Concentration', fontsize=15)
         ax.set_ylabel('Lipid Class', fontsize=15)
         ax.tick_params(axis='both', which='major', labelsize=12)
-        # Place the legend inside the plot area
         ax.legend(loc='lower right', fontsize=12)
         ax.set_title('Class Concentration Bar Chart', fontsize=15)
         for spine in ax.spines.values():
             spine.set_edgecolor('black')
-        # Use the entire plot area for the bars
         plt.tight_layout(pad=2.0)
